eebf7bf3ef7aa09fab400b7badff55c88fa3f45c
Sanjay Patel <skpatel20@gmail.com>
2021-08-29 19:22:12 +0530

    Initial commit

---
+++ spring-fusion-demo-web/src/main/java/com/naturalprogrammer/fusion/demo/web/SpringFusionDemoWebApplication.java
@@ -0,0 +1,13 @@
+package com.naturalprogrammer.fusion.demo.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFusionDemoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFusionDemoWebApplication.class, args);
+    }
+
+}
---
+++ spring-fusion-demo-web/src/test/java/com/naturalprogrammer/fusion/demo/web/SpringFusionDemoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naturalprogrammer.fusion.demo.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFusionDemoWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
